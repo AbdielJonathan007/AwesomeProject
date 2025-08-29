bedd9c1d2649ed260762504682351e1429a45ef8
AbdielJonathan007 <143036864+AbdielJonathan007@users.noreply.github.com>
2025-08-28 23:57:26 -0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL ='https://progress-buddy.up.railway.app;'
+const API_BASE_URL ='https://progress-buddy.up.railway.app';
 
 class ApiService {
   async request(endpoint, options = {}) {
